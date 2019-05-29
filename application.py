07658688947381394291614b5b9e5efed11f3720
dawityab <dawityab@gmail.com>
2019-05-29 18:36:14 +0200

    First commit

--- application.py
+++ application.py
@@ -1,29 +1,29 @@
 import os
+import sqlite3
 from flask import Flask, render_template, session, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 #from sqlalchemy.orm import sessionmaker
-#from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 from werkzeug.security import generate_password_hash, check_password_hash
 from form import RegistrationForm, LoginForm
 
 
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'r\x86/\x1d\x92\xa7\x043\x02\x97\xc6\xee\xf8\xaf\x07\x97'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/sqlite/reader_online.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/sqlite/reader.db'
 app.config["SESSION_PERMANENT"] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 
-#engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True)
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True)
 #db_session = sessionmaker(
  #   autocommit=False,
   #  autoflush=False,
   #  bind=engine)
+
+#c = engine.cursor()
 
 class User(db.Model):
     __tablename__ = "user"
@@ -34,7 +34,7 @@ class User(db.Model):
     confirm_password = db.Column('confirm_password', db.String(50), unique=False)
     book = db.relationship('Books', backref='read', lazy=True)
 
-    def __init__(self, user_email=None, username=None, password=None, confirm_password=None):
+    def __init__(self, user_email, username, password, confirm_password):
             self.user_email = user_email
             self.username = username
             self.password = generate_password_hash(password)
@@ -52,7 +52,7 @@ class Books(db.Model):
     year = db.Column('year', db.Integer,unique=False)
     user_id = db.Column('user_id', db.Integer, db.ForeignKey('user.id'),nullable=False)
 
-    def __init__(self,title=None, author=None, year=None):
+    def __init__(self,title, author, year):
             self.title = title
             self.author = author
             self.year = year
@@ -66,6 +66,7 @@ db.create_all()
 
 @app.route('/')
 def index():
+    print('we are here haha')
     image_file = url_for('static', filename="css/books.jpg")
 
     return render_template("index.html", image_file=image_file)
@@ -78,36 +79,46 @@ def show_register():
 
 @app.route('/register', methods=['POST'])
 def register():
+    print('we are here hihi')
     form = RegistrationForm(request.form)
 
     if request.method == 'POST':
      if form.user_email.data is None or form.password.data is None:
       flash("User email and password is required")
-    if User.query.filter_by(username = form.username.data).first() is not None:
+    if User.query.filter_by(username=form.username.data).first() is not None:
       flash("User name already existing")
 
-    hashed_password = generate_password_hash(form.password.data).decode('utf-8')
+    hashed_password = generate_password_hash(form.password.data)
+    print('hashed password:'+ hashed_password)
+    """user = User(request.form['user_email'],
+                request.form['username'],
+                request.form['password'],
+                request.form['confirm_password'])"""
+
     user = User(username=form.username.data,
                     user_email=form.user_email.data,
                     password=form.password.data,
                     confirm_password=hashed_password)
+
 
     db.session.add(user)
     db.session.commit()
 
     flash('Your account has been created.You can now login','success')
 
-    return redirect(url_for('afterlogin'))
-    return render_template('register.html', form=form)
+    return redirect(url_for('login'))
+    # TODO add if else statement return render_template('register.html', form=form)
 
 
 @app.route("/login", methods=["GET"])
 def login():
-    return render_template("login.html")
+    image_file = url_for('static', filename="css/college_library.jpg")
+    return render_template("login.html", image_file=image_file)
 
 
 @app.route('/login', methods=['POST'])
 def login_post():
+
     form = LoginForm(request.form)
 
     if request.method == 'POST':
@@ -151,5 +162,6 @@ def logout():
 
 
 if __name__ == "__main__":
+
     db.create_all()
     app.run(debug = True)
