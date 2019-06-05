32c7cbdd6b5dcd60e9f29044f05c02ef73f5b713
dawityab <dawityab@gmail.com>
2019-06-05 11:26:23 +0200

    Final

--- application.py
+++ application.py
@@ -3,29 +3,29 @@ import sqlite3
 from flask import Flask, render_template, session, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-#from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from werkzeug.security import generate_password_hash, check_password_hash
-from form import RegistrationForm, LoginForm
+from form import RegistrationForm, LoginForm, SearchForm
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required, UserMixin
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = 'r\x86/\x1d\x92\xa7\x043\x02\x97\xc6\xee\xf8\xaf\x07\x97'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/sqlite/reader.db'
-app.config["SESSION_PERMANENT"] = False
-app.config['SESSION_TYPE'] = 'filesystem'
+
+
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True)
-#db_session = sessionmaker(
- #   autocommit=False,
-  #  autoflush=False,
-  #  bind=engine)
 
-#c = engine.cursor()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "user"
     id = db.Column('id', db.Integer, primary_key=True)
     user_email = db.Column('user_email',db.String(50), unique=True)
@@ -39,6 +39,7 @@ class User(db.Model):
             self.username = username
             self.password = generate_password_hash(password)
             self.confirm_password = confirm_password
+
 
     def __repr__(self):
             return '<User %r>' % (self.username)
@@ -61,12 +62,19 @@ class Books(db.Model):
             return '<Books %r>' %(self.title)
 
 
-db.create_all()
+
+#db.create_all()
 
 
-@app.route('/')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/")
 def index():
-    print('we are here haha')
+
     image_file = url_for('static', filename="css/books.jpg")
 
     return render_template("index.html", image_file=image_file)
@@ -74,94 +82,87 @@ def index():
 
 @app.route("/register", methods=["GET"])
 def show_register():
-    return render_template("register.html",form=RegistrationForm())
+    return render_template("register.html", form=RegistrationForm())
 
 
-@app.route('/register', methods=['POST'])
+@app.route("/register", methods=["POST"])
 def register():
-    print('we are here hihi')
+
     form = RegistrationForm(request.form)
-
-    if request.method == 'POST':
-     if form.user_email.data is None or form.password.data is None:
-      flash("User email and password is required")
-    if User.query.filter_by(username=form.username.data).first() is not None:
-      flash("User name already existing")
-
     hashed_password = generate_password_hash(form.password.data)
-    print('hashed password:'+ hashed_password)
-    """user = User(request.form['user_email'],
-                request.form['username'],
-                request.form['password'],
-                request.form['confirm_password'])"""
+    user_email = request.form.get("user_email")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    confirm_password = hashed_password
+    user = User.query.filter_by(user_email=user_email).first()
+    if not user_email or not username:
+        flash(u'fields should not be empty', 'warning')
+        return redirect(url_for('register'))
 
-    user = User(username=form.username.data,
-                    user_email=form.user_email.data,
-                    password=form.password.data,
-                    confirm_password=hashed_password)
+    if user:
+        flash(u'Email address already exists.', 'warning')
+        return redirect(url_for('register'))
 
+    new_user = User(user_email=user_email, username=username, password=password, confirm_password=confirm_password)
 
-    db.session.add(user)
+    db.session.add(new_user)
     db.session.commit()
-
-    flash('Your account has been created.You can now login','success')
-
+    flash(u'Your account has been created.You can now login.', 'success')
     return redirect(url_for('login'))
-    # TODO add if else statement return render_template('register.html', form=form)
 
 
 @app.route("/login", methods=["GET"])
 def login():
-    image_file = url_for('static', filename="css/college_library.jpg")
-    return render_template("login.html", image_file=image_file)
+        session['next'] = request.args.get('next')
+        image_file = url_for('static', filename="css/college_library.jpg")
+        return render_template("login.html", image_file=image_file)
 
 
 @app.route('/login', methods=['POST'])
 def login_post():
+    user_email = request.form['user_email']
+    password = request.form['password']
 
-    form = LoginForm(request.form)
-
-    if request.method == 'POST':
-      if form.user_email.data is None or form.password.data is None:
-       flash("User email and password is required")
-    user = User.query.filter_by(name=request.form['user_email']).first()
-    if user:
-     if check_password_hash(user.password, form.password.data):
-      return redirect(url_for('afterlogin'))
-    flash('Invalid user name or password')
-    return render_template('login.html', form=form)
+    user = User.query.filter_by(user_email=user_email).first()
+    if not user_email or not check_password_hash(user.password, password):
+            flash(u'Invalid credentials','warning')
+            return redirect(url_for('login'))
+    login_user(user, remember=False)
+    flash(u'You were successfully logged in ','success')
+    return redirect(url_for('afterlogin'))
 
 
-@app.route("/after-login", methods=["POST","GET"])
+
+@app.route("/after-login", methods=["POST", "GET"])
+@login_required
 def afterlogin():
-    flash("Profile Page")
 
     return render_template("after-login.html")
 
 
-''''@app.route('/search')
-def searchresults(searchs):
-    results = []
-    searchs_ = searchs.data['search']
-    if searchs.data['search'] == '':
-       qry = db_session.query(Books.title)
-    results = qry.all()
-    if not results:
-        flash('No results found!')
-        return redirect('/')
-    else:
 
-       return render_template('search.html', results=results)
+@app.route('/search_results', methods=['POST'])
+@login_required
+def search_result():
+   title = request.form['title']
 
-'''
+   titles= Books.query.filter_by(title=title).first()
+   if not titles:
+       flash('0 result found','warning')
+       return redirect(url_for('afterlogin'))
+   else:
+       flash('Search result found', 'success')
+   return render_template('search_result.html', titles=titles)
 
-@app.route('/logout')
+
+@app.route('/logout', methods=['POST'])
+@login_required
 def logout():
-
+    logout_user()
     return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
 
     db.create_all()
-    app.run(debug = True)
+    app.run(debug=True)
